846587ac3b4441e2e04c69bc4fbbc50d4f924a8a
campopinillos <1207@holbertonschool.com>
2020-02-13 17:47:22 -0500

    [Update] Console file

--- console.py
+++ console.py
@@ -4,12 +4,55 @@ HBNB Console
 
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-	""" Console """
-	prompt = '> '
-	intro = "Welcome!"
+    """ Console """
+    prompt = '(hbnb) '
+    intro = "Welcome!"
+    __classes = {'BaseModel': BaseModel}
+
+    def emptyline(self):
+        """Empty line execution"""
+        pass
+
+    def do_EOF(self, line):
+        """EOF command to exit the program"""
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_create(self, line):
+        """Create command new instance"""
+        if line is None:
+            print("** class name missing **")
+        elif line not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            obj = HBNBCommand.__classes[line]()
+            obj.save()
+            print(obj.id)
+            storage.save()
+
+    def do_show(self, line):
+        """Show command display info of the id instance"""
+        if line is None:
+            print("** class name missing **")
+        elif line.split(' ')[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(line.split(' ')) < 2:
+            print("** instance id missing **")
+        else:
+            k = "{}.{}".format(line.split(' ')[0], line.split(' ')[1])
+            if k not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[k])
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
