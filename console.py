aa8234a67a824d5610d167b15f825af05a6740bc
AdrianaAriza <1296@holbertonschool.com>
2020-02-18 11:39:13 -0500

    [Add]: .others()

--- console.py
+++ console.py
@@ -145,6 +145,12 @@ class HBNBCommand(cmd.Cmd):
             lines = line.split('.')
             if lines[1] == "all()":
                 self.do_all(lines[0])
+            elif lines[1] == "count()":
+                self.do_count(lines[0])
+            elif lines[1][0:5] == "show(" and lines[1][-1:] == ")":
+                self.do_show(lines[0] + " " + lines[1][5:-1])
+            elif lines[1][0:8] == "destroy(" and lines[1][-1:] == ")":
+                self.do_destroy(lines[0] + " " + lines[1][8:-1])
         else:
             return cmd.Cmd.default(self, line)
 
