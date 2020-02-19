b75a76d8756354b7045a8f56d27c9d0cd68fd5a9
AdrianaAriza <1296@holbertonschool.com>
2020-02-19 00:14:45 -0500

    [Fix]

--- console.py
+++ console.py
@@ -158,10 +158,12 @@ class HBNBCommand(cmd.Cmd):
                     l_attr = lines[1][7:-1].split(", {")
                     id = l_attr[0]
                     d_attr = eval("{" + l_attr[1])
+                    print(d_attr)
                     for k, v in d_attr.items():
                         s_attr = s_attr + " " + id + " " + k + " " + str(v)
+                        print(s_attr)
                         self.do_update(s_attr)
-                    s_attr = str(lines[0])
+                        s_attr = str(lines[0])
                 else:
                     for i in l_attr:
                         s_attr = s_attr + " " + i
